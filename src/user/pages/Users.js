89b4d4b822f74daa63dd71f170265d2eda0632be
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-07-07 22:05:46 -0500

    passed in dummy data to users component to establish initial flow

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
+import UserList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1", 
+      name: "Chaim", 
+      image: "image1", 
+      places: "3"
+    }
+  ];
+
+  return <UserList items={USERS}/>;
 };
 
 export default Users;
