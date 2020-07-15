62036e0ce185a398d50eda81add0861b79fc528a
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-07-15 13:49:58 -0500

    wrap link in useritem with the card.

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom' 
 
-import './UserItem.css';
+import Card from '../../shared/components/UIElements/Card'; 
 import Avatar from '../../shared/components/UIElements/Avatar'
+import './UserItem.css';
 
 
 const UserItem = props => {
   return (
     <li className="user-item">
         <div className="user-item__content">
+         <Card> 
           <Link to={`/${props.id}/places`}>
             <div className="user-item__image">
                <Avatar image={props.image} alt={props.name}  />
@@ -18,6 +20,7 @@ const UserItem = props => {
   <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
             </div>
             </Link>
+           </Card>
         </div>   
      </li>
     )
