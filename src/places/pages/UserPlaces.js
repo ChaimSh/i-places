90a24907aba37b889f80d3335634b19d2fcc6ad8
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-08-10 19:08:44 -0500

    created bummy places and passed a filtered verion down placeslist inoreder to filter by user.

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {useParams} from 'react-router-dom'
+
 import PlaceList from '../components/PlaceList';
 
+
+const DUMMY_PLACES = [{
+    id: 'p1',
+    title: "empire state building",
+    description: "one of the tallest building in the world",
+    imageURL: "image of the EMpire state building",
+    address: " 20 W 31st st, NEW YORK, NY 10001",
+    lacation: {
+        lat: 40.7484,
+        lng: -73.98785
+    },
+    creator: 'U1' 
+}];
 const UserPlaces = () => {
-  return <PlaceList />;
+   const userId = useParams().userId;
+   const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+   return <PlaceList items={loadedPlaces}/>;
 };
 
 export default UserPlaces;
