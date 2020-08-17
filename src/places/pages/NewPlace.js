49b67ee0b29fada25a77ac628bf367b86bd7e026
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-08-16 22:36:58 -0500

    Input js

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+  return <form className="place-form"> 
+     <Input type="text"  label="Title" validators={[]} />
+  </form>
 };
 
 export default NewPlace;
