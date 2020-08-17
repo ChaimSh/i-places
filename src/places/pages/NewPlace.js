62409878be1fc7a01efe3070fcf565d7c5e68de1
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-08-16 22:44:11 -0500

    newplac input imported

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css"
 const NewPlace = () => {
   return <form className="place-form"> 
