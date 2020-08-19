2169045c554fb48de4b948e6251f34f99313ce45
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-08-19 18:54:18 -0500

    add useReducer to input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -4,7 +4,7 @@ import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css"
 const NewPlace = () => {
   return <form className="place-form"> 
-     <Input type="text"  label="Title" validators={[]} />
+     <Input element="input" type="text"  label="Title" validators={[]} />
   </form>
 };
 
